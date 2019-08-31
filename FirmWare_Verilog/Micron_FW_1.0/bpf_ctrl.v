333532f4efd025aa51b02320fd940213536877c7
David Fainitski <rolin791@gmail.com>
2019-08-31 11:52:50 -0700

    Add files via upload

---
+++ FirmWare_Verilog/Micron_FW_1.0/bpf_ctrl.v
@@ -0,0 +1,27 @@
+//  BPF control///
+
+
+
+module bpf_ctrl (
+   input [15:0] freq,
+	output reg [2:0] bpf
+	);
+	
+	
+always @(freq)	// freq is freqoency in Hz devided by 65536
+begin
+   if (freq <= 38) bpf <= 3'd6; // 0-2.5MHz
+	else
+	if (freq <= 91) bpf <= 3'd2; // 2.5-6.0 MHz
+	else
+	if (freq <= 191) bpf <= 3'd0; // 6.0 - 12.5 MHz
+	else
+	if (freq <= 305) bpf <= 3'd3; // 12.5 - 20.0 MHz
+	else
+	bpf <= 3'd1; // 20 - 30 MHz
+end
+
+
+
+endmodule
+
